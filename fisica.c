3b267ad6afc3a3268246a1eef024e757eff1d91c
Galoco <87337463+Galoco@users.noreply.github.com>
2021-07-23 18:10:33 -0300

    Add files via upload

---
+++ fisica.c
@@ -0,0 +1,12 @@
+#include <fisica.h>
+#include <math.h>
+
+//funciones para la pelotita
+double computar_velocidad (double vi, double a, double dt, double rozamiento){
+    return  (dt * a + vi)/rozamiento;//ESTA BIEN ESTO??
+    //return (dt*a+vi)- (dt*rozamiento) se me ocurre esto, lo consulto por discord  
+}
+
+double computar_posicion (double pi, double vi, double dt){
+    return  dt * vi + pi;
+}
